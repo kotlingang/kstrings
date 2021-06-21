a1e8f4c5adf8afc9d2e31456041f510a8651a5d7
Alex Sokol <sokolsokolinskiy@gmail.com>
2021-06-21 11:46:47 +0300

    removed redundant delegate definition and build now is public.

--- core/src/commonMain/kotlin/fun/kotlingang/kstrings/builder/LocalizedStringBuilder.kt
+++ core/src/commonMain/kotlin/fun/kotlingang/kstrings/builder/LocalizedStringBuilder.kt
@@ -3,8 +3,6 @@
 package `fun`.kotlingang.kstrings.builder
 
 import `fun`.kotlingang.kstrings.LocalizedString
-import kotlin.properties.ReadOnlyProperty
-import kotlin.reflect.KProperty
 
 
 public class LocalizedStringBuilder(private val default: String) {
@@ -14,17 +12,7 @@ public class LocalizedStringBuilder(private val default: String) {
         variants[code] = value
     }
 
-    @PublishedApi
-    internal fun build(): LocalizedString = LocalizedString(default, variants)
-
-    public inner class CodeDelegate internal constructor(private val value: String) {
-        public operator fun provideDelegate (
-            thisRef: Any?, property: KProperty<*>
-        ): ReadOnlyProperty<Any?, Unit> {
-            variants[property.name] = value
-            return ReadOnlyProperty { _, _ -> }
-        }
-    }
+    public fun build(): LocalizedString = LocalizedString(default, variants)
 
     // Codegen part
 
